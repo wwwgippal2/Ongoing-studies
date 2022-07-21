5cbee4fb98c1ef0a8f3c0a1b1d47be6aac88ee4d
Gustavo Henrique Romão <33608430+wwwgippal2@users.noreply.github.com>
2022-07-21 19:37:43 -0300

    Add files via upload

---
+++ Página_Estática_Gustavo_Romao/mail.php
@@ -0,0 +1,7 @@
+<?php
+echo "O organizador é: " . $_GET["name"];
+echo "O nome do evento é: " . $_GET["evento"];
+echo "A data e horário são:"  . $_GET["data e horário"];
+echo "Introdução do evento: " . $_GET["intro"];
+echo $showimage["fileToUpload"];
+?>
